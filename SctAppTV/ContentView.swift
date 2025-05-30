b30b8dd53fee5bef8fe9a48dfc8263d0e557b46b
Rohit.Dwivedi <rohit.dwivedi@springct.com>
2025-05-30 19:13:21 +0530

    Initial Commit

---
+++ SctAppTV/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SctAppTV
+//
+//  Created by Rohit.Dwivedi on 30/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SctAppTV/SctAppTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  SctAppTVApp.swift
+//  SctAppTV
+//
+//  Created by Rohit.Dwivedi on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SctAppTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
